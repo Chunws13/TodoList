dffdcb7cb3a6ff712a4e7d76ba8f7d7bacef6a4e
Chunws13 <dntkd51@naver.com>
2024-09-24 23:06:24 +0900

    반복 요일 버튼 추가

--- lib/Screens/create_memo.dart
+++ lib/Screens/create_memo.dart
@@ -14,6 +14,8 @@ class CreateMemo extends StatefulWidget {
 
 class _CreateMemoState extends State<CreateMemo> {
   final _formKey = GlobalKey<FormState>();
+  bool repeat = false;
+  bool alarm = false;
 
   late TextEditingController _contentControler;
 
@@ -29,6 +31,18 @@ class _CreateMemoState extends State<CreateMemo> {
     }
   }
 
+  void _repeatBtnPress() {
+    setState(() {
+      repeat = !repeat;
+    });
+  }
+
+  void _alarmBtnPress() {
+    setState(() {
+      alarm = !alarm;
+    });
+  }
+
   @override
   void dispose() {
     _contentControler.dispose();
@@ -42,11 +56,11 @@ class _CreateMemoState extends State<CreateMemo> {
         appBar: AppBar(
           title: const Text("새 메모 만들기"),
         ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
+        body: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: TextFormField(
                 controller: _contentControler,
                 decoration: const InputDecoration(labelText: "오늘 할 일"),
                 validator: (String? value) {
@@ -56,14 +70,80 @@ class _CreateMemoState extends State<CreateMemo> {
                   return null;
                 },
               ),
-              ElevatedButton(
-                onPressed: _contentSumbit,
-                child: const Text("저장"),
-              ),
-            ],
+            ),
+            const SizedBox(
+              height: 36,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _repeatBtnPress,
+                  child: const Text("반복"),
+                ),
+                ElevatedButton(
+                  onPressed: _alarmBtnPress,
+                  child: const Text("알림"),
+                ),
+              ],
+            ),
+            repeat ? const DayWidget() : const Text(""),
+            alarm ? const Text("알람 버튼 True") : const Text(""),
+          ],
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ElevatedButton(
+            onPressed: _contentSumbit,
+            child: const Text("저장"),
           ),
         ),
       ),
+    );
+  }
+}
+
+class DayWidget extends StatefulWidget {
+  const DayWidget({super.key});
+
+  @override
+  State<DayWidget> createState() => _DayWidgetState();
+}
+
+class _DayWidgetState extends State<DayWidget> {
+  Map<String, bool> days = {
+    "일": false,
+    "월": false,
+    "화": false,
+    "수": false,
+    "목": false,
+    "금": false,
+    "토": false
+  };
+
+  void dayPressed(String day) {
+    setState(() {
+      days[day] = !days[day]!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 10,
+      children: days.keys.map((day) {
+        return GestureDetector(
+          onTap: () => dayPressed(day),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundColor: days[day]! ? Colors.black : Colors.red,
+            child: Text(
+              day,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      }).toList(),
     );
   }
 }
